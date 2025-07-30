0c68314699a7939a481ca0f19735fe7f6a888206
SamuelGR231095 <samuelgr231095@gmail.com>
2025-07-30 17:47:57 -0600

    Create mfa-recovery-code-enrollment.js

---
+++ js/mfa-recovery-code-enrollment.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+        
+        const main = document.querySelector('main._widget.caa90f795'); 
+        const title = document.querySelector('h1.cded47f4b.c3871ca44');
+        const subTitle = document.querySelector('.c31b86380.ca3fde804');
+        const btnContinue = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c354a5c85');
+        const spanSavedCode = document.querySelector('.cdf1d7eb2.c2baadf66');
+        
+        main.classList.remove('_widget', 'caa90f795');        
+        title.className = 'title-container';
+        subTitle.className = 'subtitle-container';
+        btnContinue.className ='button-normal';
+        spanSavedCode.classList.add('text');
+            
+        document.getElementById('auth0-hidden-wrapper').style.display = 'block';
+
+    });
