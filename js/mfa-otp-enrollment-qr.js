0a2564520bf907d802f3f8c1f89812e74e53df32
SamuelGR231095 <samuelgr231095@gmail.com>
2025-07-30 14:01:16 -0600

    Create mfa-otp-enrollment-qr.js
    Se agrega js para pantalla de enrolamiendo con qr

---
+++ js/mfa-otp-enrollment-qr.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+        const main = document.querySelector('main._widget');
+        const title = document.querySelector('h1.cded47f4b');
+        const subTitle = document.querySelector('.c31b86380.ca3fde804');
+        const btnScan = document.querySelector('.c78dfd5b3._link-toggle-view.c7e1be07f');
+        const btnChangeMethod = document.querySelector('.c78dfd5b3.ca9aac5f4.c7e1be07f');
+        const btnLogin = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c48cd8769');
+        const labelCode = document.querySelector('.cd36df9bd.c2d2ff081.c66f4bc7e');
+        const enterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
+        const inputEnterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
+        const input = document.getElementById('code');
+
+        main.classList.remove('_widget', 'c7f43d5ad');
+        title.className = 'title-container';
+        btnLogin.className ='button-normal';
+        subTitle.className = 'subtitle-container';
+        input.className = 'email-input';
+        labelCode.className = 'c2d2ff081 c66f4bc7e';
+        enterCode.className = '.c141f6ee9 cffa611b3 text c206ae231 ulp-field';
+        btnScan.classList.add('link');
+        btnChangeMethod.classList.add('link');
+        document.getElementById('auth0-hidden-wrapper').style.display = 'block';
+
+    });
