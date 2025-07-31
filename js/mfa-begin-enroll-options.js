a30a0838ecc473860ef92d73eedc51088df09e70
jorgemondraxd <85580195+jorgemondraxd@users.noreply.github.com>
2025-07-31 11:06:16 -0700

    Update mfa-begin-enroll-options.js

--- js/mfa-begin-enroll-options.js
+++ js/mfa-begin-enroll-options.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const main = document.querySelector('main._widget.c1ea0cbeb');
     const title = document.querySelector('h1.cded47f4b');
     const subTitle = document.querySelector('.c31b86380.ca3fde804');
+    
+    if (window.innerWidth <= 600) {
+        const spaceDiv = document.querySelector('.ca40fca87.c7f03c812');
+        spaceDiv.style.setProperty('padding', '0 var(--spacing-0)', 'important');
+    }
 
     main.classList.remove('_widget', 'c1ea0cbeb');
     title.className = 'title-container';
