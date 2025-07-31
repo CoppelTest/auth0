f07b0a1526816d620e275a4012a33056ffb2336a
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-07-31 12:01:07 -0700

    Update mfa-email-challenge.js

--- js/mfa-email-challenge.js
+++ js/mfa-email-challenge.js
@@ -1,8 +1,8 @@
-//document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
   const main = document.querySelector('main._widget');
   const title = document.querySelector('h1.cded47f4b');
   const subTitle = document.querySelector('.c31b86380.ca3fde804');
 
   title.className = 'title-container';
   subTitle.className = 'subtitle-container'; 
-//});
+});
