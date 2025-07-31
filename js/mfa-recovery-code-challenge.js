b928c194e11427210abb3de00313c652c27d25c3
SamuelGR231095 <samuelgr231095@gmail.com>
2025-07-31 02:11:54 -0600

    Update mfa-recovery-code-challenge.js

--- js/mfa-recovery-code-challenge.js
+++ js/mfa-recovery-code-challenge.js
@@ -4,7 +4,8 @@
         const title = document.querySelector('h1.cded47f4b.c3871ca44');
         const subTitle = document.querySelector('.c31b86380.ca3fde804');
         const btnContinue = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c48cd8769');
-        const inputEnterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.c2f342594.c29c5cf1d.ulp-field.ca2723af5');
+        const inputEnterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
+        
         const labelCode = document.querySelector('.cd36df9bd.c2d2ff081.c66f4bc7e');
         const input = document.getElementById('code');
         const btnChangeMethod = document.querySelector('.c78dfd5b3.ca9aac5f4.c7e1be07f');
