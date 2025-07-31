414f4922761669c6796b13b29642ac1c7975329b
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-07-31 13:43:00 -0700

    Update mfa-email-challenge.js

--- js/mfa-email-challenge.js
+++ js/mfa-email-challenge.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const enterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
   const txtSend = document.querySelector('.c0d4bc756.c4d0f4b3c');
   const btnContinue = document.querySelector('div.c04b24768 button[type="submit"]');
+  const btnChangeMethod = document.querySelector('.c78dfd5b3.ca9aac5f4.c7e1be07f');
 
   main.classList.remove('_widget', 'c7f43d5ad');
   title.className = 'title-container';
@@ -15,6 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
   labelCode.className = 'c2d2ff081 c66f4bc7e';
   enterCode.className = '.c141f6ee9 cffa611b3 text c206ae231 ulp-field';
   txtSend.style.display = 'none';
+  btn.className = '';
   btn.className = 'button-normal';
   btnContinue.style.setProperty('margin-top', '45px', 'important');
+  btnChangeMethod.classList.add('link', 'link-method');
 });
