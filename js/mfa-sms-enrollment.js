34bc5572ece8dbd19f8bfd6adad60fd7a0f065e8
jorgemondraxd <85580195+jorgemondraxd@users.noreply.github.com>
2025-08-05 13:47:14 -0700

    Update mfa-sms-enrollment.js

--- js/mfa-sms-enrollment.js
+++ js/mfa-sms-enrollment.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const main = document.querySelector('main._widget.c4a74ed23');
+    const main = document.querySelector('main._widget');
     const title = document.querySelector('h1.cded47f4b');
     const subTitle = document.querySelector('.cdf1d7eb2.c2baadf66');
     main.classList.remove('_widget', 'c1ea0cbeb');
