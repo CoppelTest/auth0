f5ba34176acf964b2329640143edc66456ac5520
jorgemondraxd <85580195+jorgemondraxd@users.noreply.github.com>
2025-08-05 13:45:55 -0700

    Update mfa-begin-enroll-options.js

--- js/mfa-begin-enroll-options.js
+++ js/mfa-begin-enroll-options.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const main = document.querySelector('main._widget.c1ea0cbeb');
+    const main = document.querySelector('main._widget');
     const title = document.querySelector('h1.cded47f4b');
     const subTitle = document.querySelector('.c31b86380.ca3fde804');
     main.classList.remove('_widget', 'c1ea0cbeb');
