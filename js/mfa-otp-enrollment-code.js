5e635c264f89a3ccf1220218ad5a12ed349880b6
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-08-06 09:52:11 -0700

    Update mfa-otp-enrollment-code.js

--- js/mfa-otp-enrollment-code.js
+++ js/mfa-otp-enrollment-code.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-        const main = document.querySelector('main');
+        const mainElement = document.querySelector('main');
         const title = mainElement.querySelector('h1');
         const subTitle = mainElement.querySelector('p');
 
-        main.classList.remove('_widget', 'c7f43d5ad');
+        mainElement.classList.remove('_widget', 'c7f43d5ad');
         title.className = 'title-container';
         subTitle.className = 'subtitle-container';
 });
