92887ab73a18b3575cb357da268e5f22b4f5fb1b
jorgemondraxd <85580195+jorgemondraxd@users.noreply.github.com>
2025-08-06 12:37:18 -0700

    Update mfa-sms-challenge.js

--- js/mfa-sms-challenge.js
+++ js/mfa-sms-challenge.js
@@ -1,1 +1,23 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const logoViejo = document.getElementById('custom-prompt-logo');
+    if (logoViejo) {
+        logoViejo.remove();
+    }
+    const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
+    const main = auth0Wrapper.querySelector('main');
+    const title = auth0Wrapper.querySelector('h1');
+    const subTitle = auth0Wrapper.querySelector('main > section > div > div > header');
+    const header = auth0Wrapper.querySelector('main > section > div > div > header');
+    const divAfterHeader = auth0Wrapper.querySelector('main > section > div > div > div');
+    main.classList.remove(...main.classList);
+    header.style.setProperty('padding', '0 var(--spacing-0)', 'important');
+    divAfterHeader.style.setProperty('padding', '0 var(--spacing-0)', 'important');
+    title.className = 'title-container';
+    subTitle.className = 'subtitle-container';
+    
+    
+    if (auth0Wrapper) {
+        auth0Wrapper.style.removeProperty('display');
+        auth0Wrapper.style.setProperty('height', 'inherit', 'important');
+    }
+});
