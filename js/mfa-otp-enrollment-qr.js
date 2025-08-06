1f9a2bdceab88f4b5f8b0b629b44bd7e2dfef052
SamuelGR231095 <samuelgr231095@gmail.com>
2025-08-06 03:00:29 -0600

    Update mfa-otp-enrollment-qr.js

--- js/mfa-otp-enrollment-qr.js
+++ js/mfa-otp-enrollment-qr.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-        const main = document.querySelector('main._widget');
-        const title = document.querySelector('h1.cded47f4b');
-        const subTitle = document.querySelector('.c31b86380.ca3fde804');
-        const btnScan = document.querySelector('.c78dfd5b3._link-toggle-view.c7e1be07f');
-        const btnChangeMethod = document.querySelector('.c78dfd5b3.ca9aac5f4.c7e1be07f');
-        const btnLogin = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c48cd8769');
-        const labelCode = document.querySelector('.cd36df9bd.c2d2ff081.c66f4bc7e');
-        const inputEnterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
-        const enterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
-        const input = document.getElementById('code');
+        const main = auth0Wrapper.querySelector('main');
+        const title = auth0Wrapper.querySelector('h1');
+        //const subTitle = document.querySelector('.c31b86380.ca3fde804');
+        const subTitle = main?.querySelector('p');
+        //const btnScan = document.querySelector('.c78dfd5b3._link-toggle-view.c7e1be07f');
+        const btnScan = main?.querySelector('button[value="toggle-view"]');
+         //const btnChangeMethod = document.querySelector('.c78dfd5b3.ca9aac5f4.c7e1be07f');
+        const btnChangeMethod = main?.querySelector('button[value="pick-authenticator"]');
+        //const btnLogin = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c48cd8769');
+        const btnLogin = main?.querySelector('button[value="default"]');
+        
+        //const labelCode = document.querySelector('.cd36df9bd.c2d2ff081.c66f4bc7e');
+        const labelCode = document.querySelector('div[data-action-text][data-alternate-action-text]');
+        //const inputEnterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
+        //const enterCode = document.querySelector('.c141f6ee9.cffa611b3.text.c206ae231.ulp-field.ca2723af5');
+        //const input = document.getElementById('code');
+        const inputCode = document.querySelector('[type="text"][aria-required="true"]');
         
 
-        main.classList.remove('_widget', 'c7f43d5ad');
+        main.className = '';
         title.className = 'title-container';
         btnLogin.className ='button-normal';
         subTitle.className = 'subtitle-container';
-        input.className = 'email-input';
-        labelCode.className = 'c2d2ff081 c66f4bc7e';
-        enterCode.className = 'c141f6ee9 cffa611b3 text c206ae231 ulp-field';
+        inputCode.className = 'email-input';
+        labelCode.className = '';
+        //enterCode.className = 'c141f6ee9 cffa611b3 text c206ae231 ulp-field';
 
         btnScan.classList.add('link');
         btnChangeMethod.classList.add('link');
@@ -29,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
                 // Función que actualiza el estilo si hay error
+        /*
         function applyErrorStyles() {
                 console.log('entra a applyErrorStyles:');
                 inputEnterCode.className = 'c141f6ee9 cffa611b3 text c206ae231 c2f342594 c29c5cf1d ulp-field';
@@ -42,7 +50,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log('errorSpan:', errorSpan);
                 inputEnterCode.className = 'cffa611b3 text c206ae231 ulp-field';
         }
-
         
         // Observa cambios en las clases del error-cs-code-required
         const observer = new MutationObserver(function (mutationsList) {
@@ -57,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 applyErrorStyles();
             }
         }
+        
     });
 
     if (errorSpancs) {
@@ -64,7 +72,9 @@ document.addEventListener('DOMContentLoaded', function () {
             attributes: true,
             attributeFilter: ['class']
         });
+        
     }
+    */
         const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
             if (auth0Wrapper) {
                 auth0Wrapper.style.removeProperty('display');
