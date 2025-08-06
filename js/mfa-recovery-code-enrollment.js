0d9a81861ae6ac36c4709c0e355661e8eef97cd2
SamuelGR231095 <samuelgr231095@gmail.com>
2025-08-06 15:33:34 -0600

    Update mfa-recovery-code-enrollment.js

--- js/mfa-recovery-code-enrollment.js
+++ js/mfa-recovery-code-enrollment.js
@@ -17,8 +17,24 @@ document.addEventListener('DOMContentLoaded', function () {
         subTitle.className = 'subtitle-container';
         btnContinue.className ='button-normal';
         spanSavedCode.classList.add('text');
-
         */
+        const main = document.querySelector('main');
+        const title = document.querySelector('h1');
+        const subTitle = main?.querySelector('p');
+        const btnContinue = document.querySelector('button[value="default"]');
+        const btnCopyCode = Array.from(document.querySelectorAll('button')).find(btn => 
+          btn.querySelector('span') !== null
+        );        
+
+
+        main.className = '';
+        title.className = 'title-container';
+        subTitle.className = 'subtitle-container';
+        btnContinue.className ='button-normal';
+        if (btnCopyCode) {
+          btnCopyCode.classList.add('btn-copy-code');
+          btnCopyCode.classList.add('span-copy-style');
+        }
         const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
             if (auth0Wrapper) {
                 auth0Wrapper.style.removeProperty('display');
