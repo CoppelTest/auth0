12b0d6c858c2f1c589c24ba75ad4736557848344
SamuelGR231095 <samuelgr231095@gmail.com>
2025-08-06 03:09:04 -0600

    Update mfa-otp-enrollment-qr.js

--- js/mfa-otp-enrollment-qr.js
+++ js/mfa-otp-enrollment-qr.js
@@ -77,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
         
     }
     */
-        const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
             if (auth0Wrapper) {
                 auth0Wrapper.style.removeProperty('display');
             } 
