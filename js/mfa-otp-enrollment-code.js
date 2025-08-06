8877974988ab4eb29dba94f71d63e8ebca807f4f
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-08-06 10:28:28 -0700

    Update mfa-otp-enrollment-code.js

--- js/mfa-otp-enrollment-code.js
+++ js/mfa-otp-enrollment-code.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const txtCode = mainElement.querySelectorAll('section div > div')[5];
         const btnCopyCode = mainElement.querySelector('button[type="button"]');
         const spanCopy = btnCopyCode ? btnCopyCode.querySelector('span');
+        const btnScan = mainElement?.querySelector('button[value="toggle-view"]');
+        const btnChangeMethod = mainElement?.querySelector('button[value="pick-authenticator"]');
 
         mainElement.classList.remove('_widget', 'c7f43d5ad');
         title.className = 'title-container';
@@ -12,4 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
         txtCode.classList.add('mi-multiline');
         btnCopyCode.classList.add('btn-copy-code');
         spanCopy.classList.add('span-copy-style');
+        btnScan.classList.add('link');
+        btnChangeMethod.classList.add('link');
 });
