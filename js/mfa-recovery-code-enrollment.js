cb5a5979bfec7f9389021598aa90223dc8db3ae2
SamuelGR231095 <samuelgr231095@gmail.com>
2025-08-07 10:41:31 -0600

    Update mfa-recovery-code-enrollment.js

--- js/mfa-recovery-code-enrollment.js
+++ js/mfa-recovery-code-enrollment.js
@@ -1,23 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-        /*
-        const main = document.querySelector('main._widget.caa90f795'); 
-        const title = document.querySelector('h1.cded47f4b.c3871ca44');
-        const subTitle = document.querySelector('.c31b86380.ca3fde804');
-        const btnContinue = document.querySelector('.cea6d5264.c125e81f5.c8447a25a.ca798bf5e.c354a5c85');
-        const spanSavedCode = document.querySelector('.cdf1d7eb2.c2baadf66');
-        const btnCopyCode = document.querySelector('.cea6d5264.c6a8be725.js-required.c8447a25a.cbd8a087f.c404de425.cc0e0bd0e');
-        const inputSaved = document.querySelector('.c60279747.c4199c0da');                                       
-        const checkBox = document.querySelector('.c9b982290.c0c94efe0.c71e7db48')
-        btnCopyCode.classList.add('btn-copy-code');
-        btnCopyCode.classList.add('span-copy-style');
-        main.classList.remove('_widget', 'caa90f795');
-        inputSaved.classList.remove('c60279747');
-        checkBox.classList.add('custom-checkbox-container');
-        title.className = 'title-container';
-        subTitle.className = 'subtitle-container';
-        btnContinue.className ='button-normal';
-        spanSavedCode.classList.add('text');
-        */
+
         onst auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
         const main = document.querySelector('main');
         const title = document.querySelector('h1');
