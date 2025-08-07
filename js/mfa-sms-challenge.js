9137e6a0116893e1caf49088a5fd6c096577a8de
jorgemondraxd <85580195+jorgemondraxd@users.noreply.github.com>
2025-08-07 12:13:31 -0700

    Update mfa-sms-challenge.js

--- js/mfa-sms-challenge.js
+++ js/mfa-sms-challenge.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (logoViejo) {
         logoViejo.remove();
     }
-    
+    const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
     
     if (auth0Wrapper) {
         auth0Wrapper.style.removeProperty('display');
