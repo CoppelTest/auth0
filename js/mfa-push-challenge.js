d36f053e5595aafb74676071804cb08669302117
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-08-07 07:31:04 -0700

    Create mfa-push-challenge.js

---
+++ js/mfa-push-challenge.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const mainElement = document.querySelector('main');
+	mainElement.classList.remove('_widget', 'c7f43d5ad');
+	const title = mainElement.querySelector('h1');
+	title.className = 'title-container';
+	const subTitle = mainElement.querySelector('p');
+	subTitle.className = 'subtitle-container';
+	subTitle.style.paddingTop = '25px';
+
+	const waitingTextContainer = mainElement.querySelector('div > div > div > div > div > div');
+	waitingTextContainer.id = 'waiting-text-container';
+
+	const dotIcon = waitingTextContainer ? waitingTextContainer.querySelector('span[role="img"]') : null;
+	dotIcon.id ='icon'; 
+	
+	const linkStyle = document.createElement('style');
+	linkStyle.innerHTML = `
+    .link {
+      color: #1c42e8 !important;
+      font-size: 16px !important;
+    } `;
+	document.head.appendChild(linkStyle);
+	const button = document.querySelector('button[name="action"][value="pick-authenticator"]');
+	if (button) {
+		button.classList.add('link');
+	}
+	const footer = document.querySelector('footer');
+	footer.style.marginTop = 'auto';
+});
