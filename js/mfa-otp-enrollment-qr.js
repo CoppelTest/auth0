1792dde5059d7fcfd85b0de0d958fd5eda9bc440
SamuelGR231095 <samuelgr231095@gmail.com>
2025-08-08 14:27:16 -0600

    Update mfa-otp-enrollment-qr.js

--- js/mfa-otp-enrollment-qr.js
+++ js/mfa-otp-enrollment-qr.js
@@ -80,53 +80,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 
             }
 
-
-                // Función que actualiza el estilo si hay error
-        /*
-        function applyErrorStyles() {
-                console.log('entra a applyErrorStyles:');
-                inputEnterCode.className = 'c141f6ee9 cffa611b3 text c206ae231 c2f342594 c29c5cf1d ulp-field';
-                input.className = 'input ce861d26a c5e190e5a';
-        }
-
-        // Si ya existe error de código (ej. server-side render)
-        if (errorSpan) {
-                applyErrorStyles();
-        }else{
-                console.log('errorSpan:', errorSpan);
-                inputEnterCode.className = 'cffa611b3 text c206ae231 ulp-field';
-        }
-        
-        // Observa cambios en las clases del error-cs-code-required
-        const observer = new MutationObserver(function (mutationsList) {
-        for (const mutation of mutationsList) {
-                console.log('entra a observer:');
-            if (
-                mutation.type === 'attributes' &&
-                mutation.attributeName === 'class' &&
-                errorSpancs.classList.contains('ulp-validator-error')
-            ) {
-                 
-                applyErrorStyles();
-            }
-        }
-        
-    });
-
-    if (errorSpancs) {
-        observer.observe(errorSpancs, {
-            attributes: true,
-            attributeFilter: ['class']
-        });
-        
-    }
-    */
             if (auth0Wrapper) {
                 auth0Wrapper.style.removeProperty('display');
             } 
 
-
- 
 
     
 
