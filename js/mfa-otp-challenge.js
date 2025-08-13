9ef3c07660784b0fb9c49fdc62b3bb49d182e319
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-08-12 20:15:42 -0700

    Update mfa-otp-challenge.js

--- js/mfa-otp-challenge.js
+++ js/mfa-otp-challenge.js
@@ -1,56 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const auth0Wrapper = document.getElementById("auth0-hidden-wrapper");
+  try {
+    const main = auth0Wrapper.querySelector("main");
+    const title = auth0Wrapper.querySelector("h1");
+    const subTitle = main?.querySelector("p");
+    const btnContinue = main?.querySelector('button[value="default"]');
+    const inputEnterCode = document.querySelector("div[data-action-text]");
+    const labelCode = inputEnterCode?.querySelector("label");
+    const inputCode = document.getElementById("code");
+    const btnChangeMethod = main?.querySelector(
+      'button[value="pick-authenticator"]'
+    );
 
-document.addEventListener('DOMContentLoaded', function () {
-	const auth0Wrapper = document.getElementById('auth0-hidden-wrapper');
-	try {
-		const mainElement = document.querySelector('main');
-		mainElement.classList.remove('_widget', 'c7f43d5ad');
-		const title = mainElement.querySelector('h1');
-		title.className = 'title-container';
-		const subTitle = mainElement.querySelector('p');
-		subTitle.className = 'subtitle-container';
-		subTitle.style.paddingTop = '25px';
-		const btnLogin = mainElement?.querySelector('button[value="default"]');
-		btnLogin.className = 'button-normal';
-		const divCodeInput = mainElement.querySelector('div[data-action-text=""]');
-		const labelCode = document.querySelector('div[data-action-text][data-alternate-action-text]');
-		const inputCode = divCodeInput ? divCodeInput.querySelector('input[name="code"]') : null;
-		const label = mainElement.querySelector('#code-label');
-		const errorSpan = document.getElementById('error-element-code');
+    main.className = "";
+    title.className = "title-container";
+    subTitle.className = "subtitle-container";
+    btnContinue.className = "button-normal";
+    inputEnterCode.className = "";
+    labelCode.className = "text";
+    inputCode.className = "email-input";
+    btnChangeMethod.classList.add("link");
+    console.log("prueba3");
 
-		inputCode.className = 'email-input';
-		labelCode.className = '';
-		btnLogin.className = 'button-normal';
-		label.style.color = '#081754';
-		label.style.lineHeight = '24px';
-		if (inputCode && btnLogin) {
-			if (errorSpan && errorSpan.offsetHeight > 0) {
-				inputCode.style.border = '1px solid red';
-				btnLogin.style.marginTop = '80px';
-			} else {
-				inputCode.style.border = '1px solid #C9C9C9';
-				btnLogin.style.marginTop = '60px';
-			}
-		}
-		const linkStyle = document.createElement('style');
-		linkStyle.innerHTML = `
-		.link {
-		color: #1c42e8 !important;
-		font-size: 16px !important;
-		} `;
-		document.head.appendChild(linkStyle);
-		const button = document.querySelector('button[name="action"][value="pick-authenticator"]');
-		if (button) {
-			button.classList.add('link');
-		}
-		const footer = document.querySelector('footer');
-		footer.style.marginTop = 'auto';
+    const errorSpan = document.getElementById("error-element-code");
+    const errorSpancs = document.getElementById("error-cs-code-required");
 
-		if (auth0Wrapper) {
-			auth0Wrapper.style.removeProperty('display');
-		}
+    if (errorSpan) {
+      console.log("errorSpan:", errorSpan);
+      inputCode.style.border = "1px solid red";
+      btnContinue.style.setProperty("margin-top", "22%", "important");
+    } else {
+      console.log("errorSpan:", errorSpan);
+      inputCode.style.border = "1px solid #C9C9C9";
+    }
 
-	} catch (error) {
+    const observer = new MutationObserver(function (mutationsList) {
+      for (const mutation of mutationsList) {
+        console.log("entra a observer:");
+        if (
+          mutation.type === "attributes" &&
+          mutation.attributeName === "class" &&
+          errorSpancs.classList.contains("ulp-validator-error")
+        ) {
+          inputCode.style.border = "1px solid red";
+          btnContinue.style.setProperty("margin-top", "22%", "important");
+        }
+      }
+    });
 
-		auth0Wrapper.style.removeProperty('display');
-	}
+    if (errorSpancs) {
+      observer.observe(errorSpancs, {
+        attributes: true,
+        attributeFilter: ["class"],
+      });
+    }
+    const rememberBrowserInput = document.getElementById("rememberBrowser");
+    if (rememberBrowserInput) {
+      const parentDiv = rememberBrowserInput.closest("div");
+      if (parentDiv) {
+        parentDiv.style.display = "none";
+      }
+    }
+    if (auth0Wrapper) {
+      auth0Wrapper.style.removeProperty("display");
+    }
+  } catch (error) {
+    auth0Wrapper.style.removeProperty("display");
+  }
 });
