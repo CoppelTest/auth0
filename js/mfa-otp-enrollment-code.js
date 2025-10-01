02d6d05dc3f2fe8b5a7ab4227c2386e7fafcad3b
tanisCopp <127356736+tanisCopp@users.noreply.github.com>
2025-10-01 15:20:20 -0700

    Update mfa-otp-enrollment-code.js

--- js/mfa-otp-enrollment-code.js
+++ js/mfa-otp-enrollment-code.js
@@ -15,8 +15,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		const btnContinue = mainElement?.querySelector('button[value="default"]')
 		const label = mainElement.querySelector('#code-label');
 		const errorSpan = document.getElementById('error-element-code');
+		const newTxt = 'Revisa que el código tenga 6 dígitos';
 		const errorSpancs = document.getElementById("error-cs-code-required");
-
+		auth0Wrapper.style.fontFamily = "'Poppins', sans-serif";
 		mainElement.classList.remove('_widget', 'c7f43d5ad');
 		title.className = 'title-container';
 		subTitle.className = 'subtitle-container';
@@ -31,27 +32,29 @@ document.addEventListener('DOMContentLoaded', function () {
 		btnContinue.style.setProperty("margin-top", "22%", "important");
 		label.style.color = '#081754';
 		label.style.lineHeight = '24px';
-
+		
+		if (errorSpan && errorSpan.textContent.includes('El código OTP debe tener 6 caracteres numéricos')) {
+		    errorSpan.innerHTML = errorSpan.innerHTML.replace('El código OTP debe tener 6 caracteres numéricos', newTxt);
+		}
 		if (errorSpan) {
-      inputCode.style.border = "1px solid red";
-      btnContinue.style.setProperty("margin-top", "22%", "important");
-    } else {
-      console.log("errorSpan:", errorSpan);
-      inputCode.style.border = "1px solid #C9C9C9";
-    }
+	      inputCode.style.border = "1px solid red";
+	      btnContinue.style.setProperty("margin-top", "22%", "important");
+	    } else {
+	      inputCode.style.border = "1px solid #C9C9C9";
+	    }
 
-    const observer = new MutationObserver(function (mutationsList) {
-      for (const mutation of mutationsList) {
-        if (
-          mutation.type === "attributes" &&
-          mutation.attributeName === "class" &&
-          errorSpancs.classList.contains("ulp-validator-error")
-        ) {
-          inputCode.style.border = "1px solid red";
-          btnContinue.style.setProperty("margin-top", "22%", "important");
-        }
-      }
-    });
+	    const observer = new MutationObserver(function (mutationsList) {
+	      for (const mutation of mutationsList) {
+	        if (
+	          mutation.type === "attributes" &&
+	          mutation.attributeName === "class" &&
+	          errorSpancs.classList.contains("ulp-validator-error")
+	        ) {
+	          inputCode.style.border = "1px solid red";
+	          btnContinue.style.setProperty("margin-top", "22%", "important");
+	        }
+	      }
+	    });
 
     if (errorSpancs) {
       observer.observe(errorSpancs, {
